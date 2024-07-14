b4fa1715c88af36d4eb91e9e205a265b6b6b89f9
willy <wy406684614@163.com>
2024-07-14 18:42:39 +0800

    0714修改

--- app01/views.py
+++ app01/views.py
@@ -1,18 +1,36 @@
 from django.shortcuts import render, HttpResponse
+from django.http import JsonResponse
 from utils.send_code import send_msg
 from app01 import models
+from django import forms
+from django.views.decorators.csrf import csrf_exempt
 
+@csrf_exempt
 def send_code(request):
-    to_email = '406684614@qq.com'
-    type = 'register'
+    type = request.GET.get('type')
+    to_email = request.POST.get('email')
     send_msg(to_email, type)
-    return HttpResponse("发送成功")
+    return JsonResponse({'status':True})
 
-class UserInfo(models):
+
+class UserInfo(forms.ModelForm):
+    password = forms.CharField(label="密码", widget=forms.PasswordInput(render_value=True))
+    confirm_password = forms.CharField(label="重置密码", widget=forms.PasswordInput(render_value=True))
+    code = forms.CharField(label='验证码', widget=forms.TextInput)
+
     class Meta:
         model = models.UserInfo
-        fields = ""
-
+        fields = ['username', 'password', 'confirm_password', 'email', 'code']
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for name, field in self.fields.items():
+            if field.widget.attrs:
+                field.widget.attrs['class'] = 'form-control'
+                field.widget.attrs['placeholder'] = field.label
+            else:
+                field.widget.attrs = {'class': 'form-control', 'placeholder': field.label}
 def register(request):
-
-    return render(request, "register.html")
+    if request.method == "GET":
+        form = UserInfo()
+        return render(request, "register.html", {'form':form})
+    form2 = UserInfo(data=request.POST)
